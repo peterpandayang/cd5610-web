87b666a1cca8c4fe5858e0de0675bed3e44c7e47
bingkunyang <bingkunyang@bingkunyangdeMacBook-Pro.local>
2016-09-07 22:19:40 -0700

    commmit for go daddy

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var app = express();
+var express = require('express');   // import module
+var app = express();    // instance of express
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));     // current directory in public folder
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+})
+
 
 app.listen(port, ipaddress);
